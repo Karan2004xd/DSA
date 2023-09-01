56b3e1c7832be81be782fb2049a210ff9ff862c6
Karan2004xd <prajapatikaran2004@gmail.com>
2023-09-02 00:46:38 +0530

    changes

--- Java/src/main/java/com/DSA/Java/Arrays/Project/Demo.java
+++ Java/src/main/java/com/DSA/Java/Arrays/Project/Demo.java
@@ -4,37 +4,43 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Demo {
-    public static void main(String[] args) {
-        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
-        // int daysAboveAvg = 0, sum = 0;
+   public static void main(String[] args) {
+      int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+      // int daysAboveAvg = 0, sum = 0;
 
-        // Scanner scanner = new Scanner(System.in);
-        
-        // System.out.print("How many days temperature ? ");
-        // arr = new int[scanner.nextInt()];
+      // Scanner scanner = new Scanner(System.in);
 
-        // for (int i = 0; i < arr.length; i++) {
-        //     System.out.print("Days " + (i + 1) + "'s high temp: ");
-        //     arr[i] = scanner.nextInt();
-        //     sum += arr[i];
-        // }
+      // System.out.print("How many days temperature ? ");
+      // arr = new int[scanner.nextInt()];
 
-        // double average = sum / arr.length;
-        // System.out.println("Average = " + average);
-        
-        // for (int i = 0; i < arr.length; i++) {
-        //     if (arr[i] > average) {
-        //         daysAboveAvg++;
-        //     }
-        // }
-        
-        // System.out.println(daysAboveAvg + " days above average temperature");
-        // scanner.close();
-        
-        System.out.println(rotateMatrix(matrix).length / 2);
-    }
-    
-    static int[][] rotateMatrix(int[][] arr) {
-        return arr;
-    }
+      // for (int i = 0; i < arr.length; i++) {
+      //     System.out.print("Days " + (i + 1) + "'s high temp: ");
+      //     arr[i] = scanner.nextInt();
+      //     sum += arr[i];
+      // }
+
+      // double average = sum / arr.length;
+      // System.out.println("Average = " + average);
+
+      // for (int i = 0; i < arr.length; i++) {
+      //     if (arr[i] > average) {
+      //         daysAboveAvg++;
+      //     }
+      // }
+
+      // System.out.println(daysAboveAvg + " days above average temperature");
+      // scanner.close();
+
+      // System.out.println(rotateMatrix(matrix).length / 2);
+   }
+
+   static boolean rotateMatrix(int[][] arr) {
+      if (arr.length == 0 || arr.length != arr[0].length){
+         return false;
+      }
+      for (int i = 0; i <= arr.length / 2; i++) {
+          
+      }
+      return true;
+   }
 }
