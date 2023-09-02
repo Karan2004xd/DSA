49fa3f11d77697257f700d8c204d01b787af0c9e
Karan2004xd <prajapatikaran2004@gmail.com>
2023-09-02 14:29:21 +0530

    some more

--- Java/src/main/java/com/DSA/Java/Arrays/Project/Demo.java
+++ Java/src/main/java/com/DSA/Java/Arrays/Project/Demo.java
@@ -1,76 +1,34 @@
 package com.DSA.Java.Arrays.Project;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Demo {
    public static void main(String[] args) {
-      int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
-      // int daysAboveAvg = 0, sum = 0;
+      int[] arr;
+      int daysAboveAvg = 0, sum = 0;
 
-      // Scanner scanner = new Scanner(System.in);
+      Scanner scanner = new Scanner(System.in);
 
-      // System.out.print("How many days temperature ? ");
-      // arr = new int[scanner.nextInt()];
+      System.out.print("How many days temperature ? ");
+      arr = new int[scanner.nextInt()];
 
-      // for (int i = 0; i < arr.length; i++) {
-      //     System.out.print("Days " + (i + 1) + "'s high temp: ");
-      //     arr[i] = scanner.nextInt();
-      //     sum += arr[i];
-      // }
-
-      // double average = sum / arr.length;
-      // System.out.println("Average = " + average);
-
-      // for (int i = 0; i < arr.length; i++) {
-      //     if (arr[i] > average) {
-      //         daysAboveAvg++;
-      //     }
-      // }
-
-      // System.out.println(daysAboveAvg + " days above average temperature");
-      // scanner.close();
-
-      rotateMatrix(matrix);
-   }
-
-   static boolean rotateMatrix(int[][] arr) {
-      if (arr.length == 0 || arr[0].length != arr.length) return false;
-      int n = arr.length;
-      int last;
-
-      if (n % 2 != 0) {
-         last = n / 2 + 1;
-      } else {
-         last = n / 2;
-      }
-
-      for (int i = 0; i < n; i++) {
-         int temp;
-         for (int j = 0; j < n; j++) {
-            if (i != j && j > i) {
-               temp = arr[i][j];
-               arr[i][j] = arr[j][i];
-               arr[j][i] = temp;
-            }
-
-            if (j >= last) {
-               int first = n - j - 1;
-               temp = arr[i][first];
-               arr[i][first] = arr[i][last];
-               arr[i][last] = temp;
-               last++;
-            }
-         }
-         printMatrix(arr);
-         System.out.println();
-      }
-      return true;
-   }
-
-   static void printMatrix(int[][] arr) {
       for (int i = 0; i < arr.length; i++) {
-         System.out.println(Arrays.toString(arr[i]));
+          System.out.print("Days " + (i + 1) + "'s high temp: ");
+          arr[i] = scanner.nextInt();
+          sum += arr[i];
       }
+
+      double average = sum / arr.length;
+      System.out.println("Average = " + average);
+
+      for (int i = 0; i < arr.length; i++) {
+          if (arr[i] > average) {
+              daysAboveAvg++;
+          }
+      }
+
+      System.out.println(daysAboveAvg + " days above average temperature");
+      scanner.close();
    }
+
 }
---
+++ Java/src/main/java/com/DSA/Java/Arrays/Project/RotateMatrix.java
@@ -0,0 +1,59 @@
+package com.DSA.Java.Arrays.Project;
+import java.util.Arrays;
+
+public class RotateMatrix {
+   public static void main(String[] args) {
+      int[][] matrix = {
+         {1,2,3,4,5},
+         {6,7,8,9,10},
+         {11,12,13,14,15},
+         {16,17,18,19,20},
+         {21,22,23,24,25}
+      };
+
+      printMatrix(matrix);
+      System.out.println();
+
+      rotateMatrix(matrix);
+   }
+
+   static boolean rotateMatrix(int[][] arr) {
+      if (arr.length == 0 || arr[0].length != arr.length) return false;
+      int n = arr.length;
+      int last;
+
+
+      for (int i = 0; i < n; i++) {
+         // if (n % 2 != 0) {
+         //    last = n / 2 + 1;
+         // } else {
+         //    last = n / 2;
+         // }
+         last = (n % 2 != 0) ? n / 2 + 1 : n / 2;
+         int temp;
+         for (int j = 0; j < n; j++) {
+            if (i != j && j > i) {
+               temp = arr[i][j];
+               arr[i][j] = arr[j][i];
+               arr[j][i] = temp;
+            }
+
+            if (j >= last) {
+               int first = n - j - 1;
+               temp = arr[i][first];
+               arr[i][first] = arr[i][last];
+               arr[i][last] = temp;
+               last++;
+            }
+         }
+      }
+      printMatrix(arr);
+      return true;
+   }
+
+   static void printMatrix(int[][] arr) {
+      for (int i = 0; i < arr.length; i++) {
+         System.out.println(Arrays.toString(arr[i]));
+      }
+   }
+}
