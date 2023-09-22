4096e0555f0ab6b4626c677fe57550da8feb37d1
Karan2004xd <prajapatikaran2004@gmail.com>
2023-09-22 19:19:50 +0530

    some questions remaining

--- Java/src/main/java/com/DSA/Java/Recursion/ExerciseQuestions.java
+++ Java/src/main/java/com/DSA/Java/Recursion/ExerciseQuestions.java
@@ -1,11 +1,28 @@
 package com.DSA.Java.Recursion;
 
+import java.util.Arrays;
+
 public class ExerciseQuestions {
    public static void main(String[] args) {
-
+      System.out.println(isPalindrome("amanaplanacanalpanama"));
    }
 
-   // public String reverse(String str) {
-      
-   // }
+   public static String reverse(String str) {
+      if (str.isEmpty()) {
+         return str;
+      }
+      return reverse(str.substring(1)) + str.charAt(0);
+   }
+
+   public static boolean isPalindrome(String s) {
+      return s.equals(reverse(s));
+   }
+
+   public static boolean someRecursive(int[] arr, OddFunction isOdd) {
+      if (arr.length == 1) {
+         return isOdd.run(arr[0]);
+      }
+      return someRecursive(Arrays.copyOfRange(arr, 0, arr.length - 1), isOdd);
+   }
+
 }
