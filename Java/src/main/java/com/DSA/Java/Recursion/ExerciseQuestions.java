c1e75e5b8465ab7d63d113b1302671d6d0195392
Karan2004xd <prajapatikaran2004@gmail.com>
2023-09-22 23:15:57 +0530

    started tree and finished recursion

--- Java/src/main/java/com/DSA/Java/Recursion/ExerciseQuestions.java
+++ Java/src/main/java/com/DSA/Java/Recursion/ExerciseQuestions.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class ExerciseQuestions {
    public static void main(String[] args) {
-      System.out.println(isPalindrome("amanaplanacanalpanama"));
    }
 
    public static String reverse(String str) {
@@ -19,10 +18,22 @@ public class ExerciseQuestions {
    }
 
    public static boolean someRecursive(int[] arr, OddFunction isOdd) {
-      if (arr.length == 1) {
-         return isOdd.run(arr[0]);
+      if (arr.length == 0) {
+         return false;
+      } else if (isOdd.run(arr[arr.length - 1])) {
+         return true;
       }
       return someRecursive(Arrays.copyOfRange(arr, 0, arr.length - 1), isOdd);
+   }
+   
+   public static char firstUpperCase(String str) {
+      if (str.isEmpty()) {
+         return ' ';
+      } else if (Character.isUpperCase(str.charAt(0))) {
+         return str.charAt(0);
+      } else {
+         return firstUpperCase(str.substring(1, str.length()));
+      }
    }
 
 }
