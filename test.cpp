ef83751c251e289a1ca3e5395311521e80367926
Karan2004xd <prajapatikaran2004@gmail.com>
2023-09-25 00:02:41 +0530

    some changes

--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <climits>
+#include <string>
 
 void print_array(std::vector<int> print_array) {
    for (int i : print_array) {
@@ -12,18 +13,27 @@ void print_array(std::vector<int> print_array) {
 
 }
 
-std::vector<int> create_new_array(std::vector<int>& encoded, int first) {
-   std::vector<int> result_array {first};
-   for (int i = 0; i < encoded.size(); i++) {
-      result_array.push_back(result_array[result_array.size() - 1] ^ encoded[i]);
+std::string create_new_array(std::string s, std::vector<int>& indices) {
+   for (int i = 0; i < indices.size() - 1; i++) {
+      for (int j = 0; j < indices.size() - 1; j++) {
+         if (indices[j] > indices[j + 1]) {
+            char temp = s.at(indices[j]);
+            s.at(indices[j]) = s.at(indices[j + 1]);
+            s.at(indices[j + 1]) = temp;
+
+            int temp_1 = indices[j];
+            indices[j] = indices[j + 1];
+            indices[j + 1] = temp_1;
+         }
+      }
    }
-   return result_array;
+   return s;
 }
 
 int main() {
-   std::vector<int> test_vec {1,2,3};
-   
+   std::vector<int> test_vec {4,5,6,7,0,2,1,3};
+   std::cout << create_new_array("codeleet", test_vec) << std::endl;
    /* std::cout << sum_of_binary_indices(test_vec, 2) << std::endl; */
-   print_array(create_new_array(test_vec, 1));
+   /* print_array(create_new_array(test_vec)); */
    return 0;
 }
