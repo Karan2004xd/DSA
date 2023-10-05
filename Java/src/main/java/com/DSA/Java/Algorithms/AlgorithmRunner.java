a121a335f1583c7a3eac71399d922e6784d6311b
Karan2004xd <prajapatikaran2004@gmail.com>
2023-10-05 16:41:12 +0530

    merger sort compeleted

--- Java/src/main/java/com/DSA/Java/Algorithms/AlgorithmRunner.java
+++ Java/src/main/java/com/DSA/Java/Algorithms/AlgorithmRunner.java
@@ -2,13 +2,13 @@ package com.DSA.Java.Algorithms;
 
 import java.util.Arrays;
 
-import com.DSA.Java.Algorithms.Sorting.BucketSort;
+import com.DSA.Java.Algorithms.Sorting.MergeSort;
 
 public class AlgorithmRunner {
    public static void main(String[] args) {
-      BucketSort bSort = new BucketSort();
+      MergeSort mSort = new MergeSort();
       int[] arr = {5, 9, 3, 1, 2, 8, 4, 7, 6};
       System.out.println(Arrays.toString(arr));
-      System.out.println(Arrays.toString(bSort.sort(arr)));
+      System.out.println(Arrays.toString(mSort.sort(arr)));
    }
 }
