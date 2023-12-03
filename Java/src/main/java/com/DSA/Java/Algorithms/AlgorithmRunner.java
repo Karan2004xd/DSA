c229abd04700dc1f69d279a9855643202752003b
Karan2004xd <prajapatikaran2004@gmail.com>
2023-12-03 11:56:58 +0530

    working on quick sort

--- Java/src/main/java/com/DSA/Java/Algorithms/AlgorithmRunner.java
+++ Java/src/main/java/com/DSA/Java/Algorithms/AlgorithmRunner.java
@@ -1,14 +1,9 @@
 package com.DSA.Java.Algorithms;
 
-import java.util.Arrays;
-
-import com.DSA.Java.Algorithms.Sorting.MergeSort;
+import com.DSA.Java.Algorithms.Sorting.QuickSort;
 
 public class AlgorithmRunner {
-   public static void main(String[] args) {
-      MergeSort mSort = new MergeSort();
-      int[] arr = {5, 9, 3, 1, 2, 8, 4, 7, 6};
-      System.out.println(Arrays.toString(arr));
-      System.out.println(Arrays.toString(mSort.sort(arr)));
-   }
+  public static void main(String[] args) {
+    QuickSort qs = new QuickSort();
+  }
 }
---
+++ Java/src/main/java/com/DSA/Java/Algorithms/Sorting/QuickSort.java
@@ -0,0 +1,15 @@
+package com.DSA.Java.Algorithms.Sorting;
+
+public class QuickSort {
+  public int[] sort(int[] arr) {
+     
+    return arr;
+  }
+  private int partition(int[] arr, int left, int right) {
+    return -1;
+  }
+
+  private void quickSort(int[] arr, int left, int right) {
+    
+  }
+}
